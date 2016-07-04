c2fb5243689baaa5c2668bd4bbbec0862fcc3bcb
crystalDf <chendong333@gmail.com>
2016-07-04 23:22:54 +0800

    Chapter 09 Organizing Files

--- copy_file.py
+++ copy_file.py
@@ -1,6 +1,9 @@
 import shutil
 import os
 
-print(shutil.copy(os.path.join(os.getcwd(), 'Test/c.txt'), os.getcwd()))
+print(shutil.copy(os.path.join(os.getcwd(), 'test/c.txt'), os.getcwd()))
 print(shutil.copy(os.path.join(os.getcwd(), 'a.txt'),
                   os.path.join(os.getcwd(), 'b.txt')))
+
+print(shutil.copytree(os.path.join(os.getcwd(), 'test'),
+                      os.path.join(os.getcwd(), 'test/inner_test2')))
