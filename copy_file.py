1513a7acf672d5e1121070e1e35a7ce246dd0313
crystalDf <chendong333@gmail.com>
2016-07-06 21:55:27 +0800

    Chapter 09 Organizing Files

--- copy_file.py
+++ copy_file.py
@@ -1,15 +1,19 @@
 import shutil
 import os
 
-print(shutil.copy(os.path.join(os.getcwd(), 'test/c.txt'), os.getcwd()))
-print(shutil.copy(os.path.join(os.getcwd(), 'a.txt'),
-                  os.path.join(os.getcwd(), 'c.txt')))
+# print(shutil.copy(os.path.join(os.getcwd(), 'test/c.txt'), os.getcwd()))
+# print(shutil.copy(os.path.join(os.getcwd(), 'a.txt'),
+#                   os.path.join(os.getcwd(), 'c.txt')))
+#
+# print(shutil.copytree(os.path.join(os.getcwd(), 'test'),
+#                       os.path.join(os.getcwd(), 'test/inner_test2')))
+#
+# print(shutil.move(os.path.join(os.getcwd(), 'a.txt'),
+#                   os.path.join(os.getcwd(), 'test')))
+#
+# print(shutil.move(os.path.join(os.getcwd(), 'b.txt'),
+#                   os.path.join(os.getcwd(), 'test/bb.txt')))
 
-print(shutil.copytree(os.path.join(os.getcwd(), 'test'),
-                      os.path.join(os.getcwd(), 'test/inner_test2')))
-
-print(shutil.move(os.path.join(os.getcwd(), 'a.txt'),
-                  os.path.join(os.getcwd(), 'test')))
-
-print(shutil.move(os.path.join(os.getcwd(), 'b.txt'),
-                  os.path.join(os.getcwd(), 'test/bb.txt')))
+# print(os.unlink(os.path.join(os.getcwd(), 'test/bb.txt')))
+# print(os.rmdir(os.path.join(os.getcwd(), 'test/inner_test')))
+print(shutil.rmtree(os.path.join(os.getcwd(), 'test/inner_test')))
