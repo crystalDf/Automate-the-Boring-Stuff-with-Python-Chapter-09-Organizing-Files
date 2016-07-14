451bdaed9917f83426d9a5285842a603b0f5cebf
crystalDf <chendong333@gmail.com>
2016-07-14 22:04:53 +0800

    Chapter 09 Organizing Files

--- renameDates.py
+++ renameDates.py
@@ -24,17 +24,22 @@ for amerFilename in os.listdir('.'):
     if mo is None:
         continue
 
-    # Get the different parts of the filename
+    # Get the different parts of the filename.
     beforePart = mo.group(1)
     monthPart = mo.group(2)
     dayPart = mo.group(4)
     yearPart = mo.group(6)
     afterPart = mo.group(8)
 
+    # Form the European-style filename.
+    euroFilename = beforePart + dayPart + '-' + monthPart + '-' + yearPart + \
+                   afterPart
 
+    # Get the full, absolute file paths.
+    absWorkingDir = os.path.abspath('.')
+    amerFilename = os.path.join(absWorkingDir, amerFilename)
+    euroFilename = os.path.join(absWorkingDir, euroFilename)
 
-# TODO: Skip files without a date.
-# TODO: Get the different parts of the filename.
-# TODO: Form the European-style filename.
-# TODO: Get the full, absolute file paths.
-# TODO: Rename the files.
+    # Rename the files.
+    print('Renaming "%s" to "%s"...' % (amerFilename, euroFilename))
+    #shutil.move(amerFilename, euroFilename)
